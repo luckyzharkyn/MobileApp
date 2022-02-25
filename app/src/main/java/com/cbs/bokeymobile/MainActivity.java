83b3c99cdb503d6ef43be06c85d90e163fe62ef0
Zharkyn <Lucky_zharkyn@mail.ru>
2022-02-25 12:04:55 +0600

    добавлена возможность принудительного обновления

--- app/src/main/java/com/cbs/bokeymobile/MainActivity.java
+++ app/src/main/java/com/cbs/bokeymobile/MainActivity.java
@@ -2,20 +2,60 @@ package com.cbs.bokeymobile;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.play.core.appupdate.AppUpdateInfo;
+import com.google.android.play.core.appupdate.AppUpdateManager;
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory;
+import com.google.android.play.core.install.InstallState;
+import com.google.android.play.core.install.InstallStateUpdatedListener;
+import com.google.android.play.core.install.model.AppUpdateType;
+import com.google.android.play.core.install.model.InstallStatus;
+import com.google.android.play.core.install.model.UpdateAvailability;
+import com.google.android.play.core.tasks.OnSuccessListener;
+
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+
 
 public class MainActivity extends Activity {
+
+    private AppUpdateManager mAppUpdateManager;
+    private static final int RC_APP_UPDATE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button swith = (Button)findViewById(R.id.kazbutton);
-        Button swith2 = (Button)findViewById(R.id.rusbutton);
-        Button swith3 = (Button)findViewById(R.id.updateApp);
+        mAppUpdateManager = AppUpdateManagerFactory.create(this);
+        mAppUpdateManager.getAppUpdateInfo().addOnSuccessListener(new OnSuccessListener<AppUpdateInfo>() {
+            @Override
+            public void onSuccess(AppUpdateInfo result) {
+                if (result.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
+                        && result.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)) {
+                    try {
+                        mAppUpdateManager.startUpdateFlowForResult(result, AppUpdateType.IMMEDIATE, MainActivity.this,
+                                RC_APP_UPDATE);
+                    } catch (IntentSender.SendIntentException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+//        mAppUpdateManager.registerListener(installStateUpdatedListener);
+
+
+        Button swith = (Button) findViewById(R.id.kazbutton);
+        Button swith2 = (Button) findViewById(R.id.rusbutton);
 
         swith.setOnClickListener(new View.OnClickListener() {
 
@@ -35,15 +75,66 @@ public class MainActivity extends Activity {
                 startActivity(SecAct2);
             }
         });
-        swith3.setOnClickListener(new View.OnClickListener() {
 
+    }
+
+    private InstallStateUpdatedListener installStateUpdatedListener = new InstallStateUpdatedListener() {
+        @Override
+        public void onStateUpdate(InstallState state) {
+            if (state.installStatus() == InstallStatus.DOWNLOADED) {
+                showCompletedUpdate();
+            }
+
+        }
+
+
+
+        private void showCompletedUpdate() {
+            Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content), "New app is ready!",
+                    Snackbar.LENGTH_INDEFINITE);
+            snackbar.setAction("Install", new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    mAppUpdateManager.completeUpdate();
+                }
+            });
+            snackbar.show();
+        }
+
+
+
+    };
+    @Override
+    protected void onStop() {
+//        if (mAppUpdateManager != null) {
+//            mAppUpdateManager.unregisterListener(installStateUpdatedListener);
+//        }
+        super.onStop();
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RC_APP_UPDATE && resultCode != RESULT_OK) {
+            Toast.makeText(this, "Отмена", Toast.LENGTH_SHORT).show();
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mAppUpdateManager.getAppUpdateInfo().addOnSuccessListener(new OnSuccessListener<AppUpdateInfo>() {
             @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                Intent SecAct3 = new Intent(getApplicationContext(), UpdateActivity.class);
-                startActivity(SecAct3);
+            public void onSuccess(AppUpdateInfo result) {
+                if (result.updateAvailability() == UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS) {
+                    try {
+                        mAppUpdateManager.startUpdateFlowForResult(result, AppUpdateType.IMMEDIATE, MainActivity.this,
+                                RC_APP_UPDATE);
+                    } catch (IntentSender.SendIntentException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
         });
     }
-
 }
+
